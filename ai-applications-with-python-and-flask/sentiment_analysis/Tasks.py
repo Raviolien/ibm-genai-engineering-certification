be14cc5133b5e6412999e0a7266406ae28ab1d60
Raviolien <tine@vist.nu>
2025-06-03 10:25:40 +0200

    Course 5 sentiment analysis task 4
    Package the application, i.e. move into SentimentAnalysis folder, created __init__py file. Added to Tasks file lines for testing in the terminal

--- ai-applications-with-python-and-flask/sentiment_analysis/Tasks.py
+++ ai-applications-with-python-and-flask/sentiment_analysis/Tasks.py
@@ -9,3 +9,9 @@ sentiment_analyzer("I love this new technology")
 python3.11
 from sentiment_analysis import sentiment_analyzer
 sentiment_analyzer("I love this new technology")
+
+# Task 4 
+## Package the application
+python3.11
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
+sentiment_analyzer("This is fun.")
