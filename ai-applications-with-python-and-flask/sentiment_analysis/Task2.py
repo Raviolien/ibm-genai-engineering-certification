8767274cad5a55f267c8fdec387f717cad5ad317
Raviolien <tine@vist.nu>
2025-06-03 10:02:10 +0200

    Course 5 sentiment analysis task 2
    Renamed file and finalized code

--- ai-applications-with-python-and-flask/sentiment_analysis/Task2.py
+++ ai-applications-with-python-and-flask/sentiment_analysis/Task2.py
@@ -1,3 +1,4 @@
+# Create Python shell and test connection in sentiment analyser function
+python3.11
 from sentiment_analysis import sentiment_analyzer
-
 sentiment_analyzer("I love this new technology")
